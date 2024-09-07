57df9080e7396956f31718e8fdc569a0879b04f2
adilraees786 <adilraees911@gmail.com>
2024-09-07 11:37:42 +0500

    firstreactapp

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function Firstapp(){
+return(
+  <div className=" text-center ">
+    <p className="mt-5  text-yellow-400">Hi! My Name is</p>
+    <h1 className=" mt-3 text-6xl font-extrabold uppercase">Adil Raees</h1>
+    <p className="mt-2 text-red-600">Here is My First React Project</p>
+  </div>
+
+)
+}
+export default Firstapp;
